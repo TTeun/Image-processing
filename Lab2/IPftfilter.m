4dded14fdb94b1870403e9da119d34996ebff920
TTeun <teunverstraaten@gmail.com>
2016-12-07 18:36:20 +0100

    assa

--- Lab2/IPftfilter.m
+++ Lab2/IPftfilter.m
@@ -1,23 +1,38 @@
+% This function takes an input image x, and a filter kernel which is
+% given in the frequency domain as  a transfer function H.
+
 function [image] = IPftfilter(x, H)
+
     % Convert to double
     y = im2double(x);
     
     % Determine size input image
     [ M, N ] = size(y);
+    
+    % Determine padded sizes
+    P = 2 * M;
+    Q = 2 * N;
+    
+    % Pad is filled with zeros
+    pad = zeros(P,Q);
+    
+    % Put image in top left corner of pad
+    pad(1:M, 1:N) = y;
 
     % Calculate the 2D Fourier transform
-    fft_image = fft2(y);
+    fft_image = fft2(pad);
     
-    % Shift the spectrum
+    % Shift the spectrum so that it macthes layout of the kernel. Kernel
+    % has high values near its center.
     fft_image = fftshift(fft_image);
        
+    % Perform the convolution of shifted spectrum with the Gaussian
+    % Low-Pass Transfer Function
     fft_image = H.* fft_image;
 
     % Perform inverse operations
-     fft_image = ifftshift(fft_image);
+    fft_image = ifftshift(fft_image);
     fft_image = real(ifft2(fft_image));
     
-     
-    imshow(fft_image);
-
+    image = fft_image(1:M, 1:N);
 end
--- Lab2/IPgaussian.m
+++ Lab2/IPgaussian.m
@@ -1,16 +1,22 @@
+% This function computes the Gaussian Low Pass Transfer function, given a
+% cut off frequency D0, and the dimension M, N. The returned matrix will
+% have dimensions [2*M, 2*N]
+
 function [H] = IPgaussian(D0, M, N)
   % Obtain the padded sizes
-  P = M;
-  Q = N;
-  % This matrix will be D(u,v).^2
-  D_sq = zeros(M, N);
-  % We fill it first with the squares of the distances
-  for i = 1:M
-    for j = 1:N
+  P = 2*M;
+  Q = 2*N;
+
+  D_sq = zeros(P, Q);
+  % D_sq will be equal to D(u,v).^2, where D(u,v) is the distance from 
+  % point (u,v) to the center point (P/2, Q/2).
+  for i = 1:P
+    for j = 1:Q
       D_sq(i,j) = (i - P / 2)^2 + (j - Q / 2)^2;
     end
   end
-  % Then we compute H(u,v) = exp(-D^2 / 2 * D0^2)
+  
+  % Then we compute H(u,v) = exp(-D(u,v)^2 / 2 * D0^2) =
+  % exp(-D_sq / 2 * D0 * D0)
   H = exp( -D_sq / (2 * D0 * D0) );
-
 end
