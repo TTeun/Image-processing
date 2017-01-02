7ed3c87d3f85bf3cff938627d0f095f6d8398b13
bramlap92 <bramlap@gmail.com>
2017-01-02 11:02:34 +0100

    most recent

--- Lab4/IPdilate.m
+++ Lab4/IPdilate.m
@@ -3,20 +3,20 @@
 function [dilated_img] = IPdilate(img, se )
     [M , N ] = size(img) ;
     new_img = zeros(M+2,N+2); 
-    se_com = rot90(se,2);
+    se_flip = rot90(se,2);
     
     for i = 2:M+1
         for j = 2:N+1
             if img(i-1,j-1) == 1
-                new_img(i-1,j-1) = se_com(1,1);
-                new_img(i-1,j) = se_com(1,2);
-                new_img(i-1,j+1) = se_com(1,3);
-                new_img(i,j-1) = se_com(2,1);
-                new_img(i,j) = se_com(2,2);
-                new_img(i,j+1) = se_com(2,3);
-                new_img(i+1,j-1) = se_com(3,1);
-                new_img(i+1,j) = se_com(3,2);
-                new_img(i+1,j+1) = se_com(3,3);        
+                new_img(i-1,j-1)    = se_flip(1,1);
+                new_img(i-1,j)      = se_flip(1,2);
+                new_img(i-1,j+1)    = se_flip(1,3);
+                new_img(i,j-1)      = se_flip(2,1);
+                new_img(i,j)        = se_flip(2,2);
+                new_img(i,j+1)      = se_flip(2,3);
+                new_img(i+1,j-1)    = se_flip(3,1);
+                new_img(i+1,j)      = se_flip(3,2);
+                new_img(i+1,j+1)    = se_flip(3,3);        
             end
         end
     end
