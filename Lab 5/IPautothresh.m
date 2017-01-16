cc139f9b591b537e061114dc0314c158b29438ce
bramlap92 <bramlap@gmail.com>
2017-01-16 16:51:47 +0100

    20170116

---
+++ Lab 5/IPautothresh.m
@@ -0,0 +1,48 @@
+% The algorithm iteratively determines the best global threshold of input 
+% image 'img' by binning the pixel values into two bins. Then taking the 
+% mean of these bins, which are use to calculate a new threshold value T.
+% When the difference between the previous T and new T is zero, the best
+% value of T is attained and the algorithm stops. 
+
+function [output, T] = IPautothresh(img)
+    % Determine image size
+    [M,N] = size(img);
+    % Convert to double, since 
+    img_double = im2double(img);
+    % Reshape image to 1D array, make looping easier
+    img_reshape = reshape(img_double, 1, M*N);
+    
+    % Initial estimate of T
+    T_old = 0.5 ;
+    % Setting dT (the stop criteria) to a non zero value
+    dT = 10 ;
+    
+    % Automated threshold selection
+    while dT ~= 0
+        % Creating empty 'bins'
+        G1 = [];
+        G2 = [];
+        % Looping and binning of the pixels values in 1D array
+        for i = 1 : M*N
+            if img_reshape(i) > T_old
+                G1(end+1) = img_reshape(i);
+            end
+            if img_reshape(i) <= T_old 
+                G2(end+1) = img_reshape(i);
+            end
+        end
+        % Determinig the mean of the bins
+        m1 = mean(G1);
+        m2 = mean(G2);
+        % Calculating new threshold value T
+        T_new = 0.5 * ( m1 + m2 ) ;
+        % Determining dT
+        dT = T_new-T_old ;
+        % Set current T values as the previous T value
+        T_old = T_new ;
+    end
+    % Making a binary image of input image with the obtained T value, then
+    % outputting the result.
+    T = int8( T_old*255 );
+    output = imbinarize(img_double, T_old) ;
+end
---
+++ Lab 5/IPtexturemeasures.m
@@ -0,0 +1,41 @@
+function [mean, stand_dev, R, skew, U, avg_ent] = IPtexturemeasures(img)
+    % Determine image size
+    [M,N] = size(img);
+    % Reshape image to 1D array
+    img_reshape = reshape(img, 1, M * N );
+    
+    % Own histogram function because histcounts does not seem to work
+    % properly
+    hist_img = zeros(1, 256); 
+    bins = 1 : 256 ;
+    for i = 1 : M*N
+       I_val = img_reshape(i) ;
+       hist_img(I_val+1) = hist_img(I_val+1) + 1 ;
+    end 
+    % Normalize histogram    
+    hist_norm = hist_img / sum(hist_img) ;
+    
+    % Texture measures
+    % First moment: mean
+    mean = sum( hist_norm .* bins ) ;
+    
+    % Second moment: variance and R
+    var = sum( (bins - mean).^2 .* hist_norm ) ; 
+    stand_dev = var^(1/2) ;
+    var_norm = var / (255)^2 ;
+    R = 1 - 1 / ( 1 + var_norm ) ;
+    
+    % Third moment: skewness (normalized)
+    skew = sum( (bins - mean).^3 .* hist_norm ) / (255^3) ;
+    
+    % Uniformity
+    U = sum( hist_norm.^2 ) ;
+    
+    % Average entropy
+    avg_ent = 0 ;
+    for i = 1 : 256
+        if hist_norm( i ) > 0
+            avg_ent = avg_ent - hist_norm( i ) * log2(hist_norm( i )) ;
+        end
+    end
+end
